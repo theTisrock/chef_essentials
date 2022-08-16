d379ce139a201fee8916e26d83d73a15049d0994
default user made explicit <ubuntu@ip-172-31-94-61.ec2.internal>
2022-08-16 01:02:46 +0000

    define python build atttributes

--- udemy_demo/attributes/default.rb
+++ udemy_demo/attributes/default.rb
@@ -1,4 +1,6 @@
 default['udemy_demo']['companyName'] = "The Tisrock Whiskey company from attribute file"
-default['udemy_demo']['python_version'] = '3.10.0'
-
+default['udemy_demo']['python_build']['openssl']['url'] = 'https://www.openssl.org/source/'
+default['udemy_demo']['python_build']['openssl']['resource'] = 'openssl-1.1.1q'
+default['udemy_demo']['python_build']['python_version'] = '3.10.5'
+default['udemy_demo']['python_build']['url'] = 'https://www.python.org/ftp/python/'
 
